22c2d4d17ed869d38983e63852e975af2f63b272
André Monteiro <andrelmv@gmail.com>
2018-02-28 18:43:17 +0000

    Classe formatada.

--- src/pa/calc/Calculadora.java
+++ src/pa/calc/Calculadora.java
@@ -4,21 +4,21 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		// OperEx Example
-		OperEx opex=new OperEx();
+		OperEx opex = new OperEx();
 		double resEx = opex.oper(1, 2);
 		System.out.println(resEx);
 
 		// ToDo - All Students Must Implement one Operation
-		// - Oper02 - Bruno Rego  - modulo
-		Oper02 ope02=new Oper02();
+		// - Oper02 - Bruno Rego - modulo
+		Oper02 ope02 = new Oper02();
 		double res02 = ope02.oper(4, 3);
 		System.out.println(res02);
-		
+
 		// ToDo - All Students Must Implement one Operation
-				// - Oper09 - André Monteiro  - Multiplicação
-				Oper09 ope09 = new Oper09();
-				double res09 = ope09.oper(5, 3);
-				System.out.println(res09); 
+		// - Oper09 - André Monteiro - Multiplicação
+		Oper09 ope09 = new Oper09();
+		double res09 = ope09.oper(5, 3);
+		System.out.println(res09);
 
 	}
 
